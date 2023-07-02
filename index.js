717aa04144e91eee64946fb8fbae5b7944feabf0
Elizabeth <ek@elizabethkillgore.com>
2023-07-02 14:25:13 -0500

    Arrow functions

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = function() {
+    let a = 2000
+    let b = 100   
+    return a/b  
+}
+divide(2000, 100)
+
+const square = x => x ** 2
+
+square
+
+const add = (x, y) => x + y
+
+add(3,4)
+
